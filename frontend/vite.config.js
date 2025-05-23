c574f2528927eb64adb3c0757e511b712e7310c5
LeeJungeun <gomgomno@naver.com>
2025-05-23 18:27:37 +0900

    2nd GitHub deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/task-manager-app/',
+  base: '/task-manager-app/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
